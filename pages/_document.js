c72415387d95584dc7be21f808706a2cceabf05f
Gholamreza Dar <rezadar1378@gmail.com>
2021-01-09 08:42:50 +0330

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,52 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="theme-color" content="#ff0000" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          {/* Google Fonts */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
+          />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
+          />
+          {/* CSS Reset */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+          />
+          {/* Milligram CSS */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"
+          />
+          <link rel="stylesheet" href="styles.css" />
+          <title>Gholamrezadar | Personal Blog</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
